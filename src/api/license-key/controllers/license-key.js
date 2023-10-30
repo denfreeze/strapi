0eb1c1c7d7748f479030d6a87b3c5679f9aa1dc5
denfreeze <103926466+denfreeze@users.noreply.github.com>
2023-10-30 14:26:17 +0300

    Delete src/api/license-key directory

--- src/api/license-key/controllers/license-key.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * license-key controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::license-key.license-key');
--- src/api/license-key/routes/license-key.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * license-key router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::license-key.license-key');
