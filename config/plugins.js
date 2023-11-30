92262bbaebf833bac38371b7bf64da31c56cc5b2
denfreeze <gm.denbrown2007@gmail.com>
2023-11-30 13:42:46 +0300

    Update plugins.js

--- config/plugins.js
+++ config/plugins.js
@@ -6,9 +6,11 @@ module.exports = ({ env }) => ({
     },
     upload: {
       config: {
-        provider: 'local',
+        provider: 'cloudinary',
         providerOptions: {
-          sizeLimit: 10000000,
+          cloud_name: env('CLOUDINARY_NAME'),
+          api_key: env('CLOUDINARY_KEY'),
+          api_secret: env('CLOUDINARY_SECRET'),
         },
         actionOptions: {
           upload: {},
